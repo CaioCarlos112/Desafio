59e97ecba6b7053b142a9e56c08879267db254e7
CaioCarlos112 <90101099+CaioCarlos112@users.noreply.github.com>
2021-10-04 15:41:00 -0300

    correcao de nomes

--- db.js
+++ db.js
@@ -4,24 +4,24 @@ mongoClient.connect("mongodb://localhost")
             .catch(err => console.log(err))
  
 function findAll() {
-  return global.conn.collection("customers").find().toArray();
+  return global.conn.collection("funcionarios").find().toArray();
 }
              
 function insert(customer) {
-    return global.conn.collection("customers").insertOne(customer);
+    return global.conn.collection("funcionarios").insertOne(customer);
 }
  
 const ObjectId = require("mongodb").ObjectId;
 function findOne(id) {
-    return global.conn.collection("customers").findOne(new ObjectId(id));
+    return global.conn.collection("funcionarios").findOne(new ObjectId(id));
 }
  
 function update(id, customer) {
-    return global.conn.collection("customers").updateOne({ _id: new ObjectId(id) }, { $set: customer });
+    return global.conn.collection("funcionarios").updateOne({ _id: new ObjectId(id) }, { $set: customer });
 }
  
 function deleteOne(id) {
-    return global.conn.collection("customers").deleteOne({ _id: new ObjectId(id) });
+    return global.conn.collection("funcionarios").deleteOne({ _id: new ObjectId(id) });
 }
  
 module.exports = { findAll, insert, findOne, update, deleteOne }
